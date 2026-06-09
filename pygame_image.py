3c5900bc736563d5b03e0ae604caa6129bad1916
c0c2501023 <c0c2501023@edu.teu.ac.jp>
2026-06-09 16:28:20 +0900

    演習課題1

--- pygame_image.py
+++ pygame_image.py
@@ -21,8 +21,9 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
+
         key_lst = pg.key.get_pressed()
+        kk_rct.move_ip(-1,0)#演習1
         if key_lst[pg.K_UP]:
             kk_rct.move_ip(0,-1)
         if key_lst[pg.K_DOWN]:
@@ -30,7 +31,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip(-1,0)
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(1,0)
+            kk_rct.move_ip(2,0)#演習1
 
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])
